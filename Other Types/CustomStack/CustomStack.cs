04907a1457e9c5753dc295b971d58d9e215e26f3
DareDevil23 <ivailonikolov36@gmail.com>
2017-04-14 17:48:57 +0300

    added new project

---
+++ Other Types/CustomStack/CustomStack.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class CustomStack<T> where T : IComparable<T> 
+    {
+        private const int DefaultCapacity = 16;
+        private T[] elements;
+        private int currentIndex;
+
+        public CustomStack(int capacity = DefaultCapacity) 
+        {
+            this.elements = new T[capacity];
+            this.currentIndex = 0;
+        }
+
+        public int Count
+        {
+            get{ return this.currentIndex; }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.currentIndex == 0;
+            }
+        }
+
+        public void Push(T element)
+        {
+            if (this.currentIndex >= this.elements.Length)
+            {
+                this.Resize();
+            }
+            this.elements[this.currentIndex] = element;
+            this.currentIndex++;
+        }
+
+        public T Pop()
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            T topElement = this.elements[this.currentIndex - 1]; 
+            this.elements[this.currentIndex - 1] = default(T);            
+            this.currentIndex--;
+
+            return topElement;
+        }
+        public T Min()
+        {
+            T minElement = this.elements[0];
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                if (this.elements[i].CompareTo(minElement) < 0)
+                {
+                    minElement = this.elements[i];
+                }
+            }
+
+            return minElement;
+        }
+
+        public T Max()
+        {
+            T maxElement = this.elements[0];
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                if (maxElement.CompareTo(this.elements[i]) < 0)
+                {
+                    maxElement = this.elements[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                this.elements[i] = default(T);
+            }
+            this.currentIndex = 0; 
+        }
+
+        public bool Contains(T element)
+        {
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                T currentElement = this.elements[i];
+                if (currentElement.Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.elements.Take(this.currentIndex)); 
+        }
+
+        private void Resize()
+        {
+            T[] newElements = new T[this.elements.Length * 2]; 
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                newElements[i] = this.elements[i];
+            }
+
+            this.elements = newElements;
+        }
+    }
+}
---
+++ Other Types/CustomStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new CustomStack<int>();
+            stack.Push(5);
+            stack.Push(255);
+            Console.WriteLine(stack.IsEmpty); // false
+            stack.Pop();
+            Console.WriteLine(stack.Count); //1
+            Console.WriteLine(stack); //5
+            stack.Clear();
+            Console.WriteLine(stack); //empty
+            stack.Push(345666667);
+            stack.Push(456);
+            stack.Push(44445);
+            Console.WriteLine(stack.Min()); //456
+            Console.WriteLine(stack.Max()); //345666667
+            stack.Clear();
+            Console.WriteLine(stack);       //empty
+            Console.WriteLine(stack.IsEmpty); //true 
+        }
+    }
+}
